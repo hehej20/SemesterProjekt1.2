22efbf47dd85c0a67f21043c174b939f9c7e48ec
EmilieKilde <milleme@live.dk>
2023-11-14 14:12:34 +0100

    Game og inventroy
    Inventory fik fjernet gammel kode
    og game er opdateret

--- Game.java
+++ Game.java
@@ -1,35 +1,49 @@
 /* Main class for launching the game
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Game {
   static World    world    = new World();
+  static PointSystem pointSystem = new PointSystem();
+  static ArrayList<String> inventory = new ArrayList<>();
   static Context  context  = new Context(world.getEntry());
   static Command  fallback = new CommandUnknown();
   static Registry registry = new Registry(context, fallback);
   static Scanner  scanner  = new Scanner(System.in);
-  
+  static Trash trash = new Trash();
+
   private static void initRegistry () {
-    Command cmdExit = new CommandExit();
-    registry.register("exit", cmdExit);
-    registry.register("quit", cmdExit);
-    registry.register("bye", cmdExit);
-    registry.register("go", new CommandGo());
+    registry.register("quit", new CommandExit());
+    registry.register("gå", new CommandGo());
     registry.register("help", new CommandHelp(registry));
+    //Ikke helt sikker på at nedenstående skal bruges endnu
+    //registry.register("tag", new CommandGrabItem());
+    //registry.register("sorter", new CommandSortTrash());
   }
   
   public static void main (String args[]) {
-    System.out.println("Welcome to the World of Zuul!");
+    System.out.println("Velkommen til stranden! Hjælp skildpadden med opgaverne og sorter skrald for at få point :));
     
-    initRegistry();
-    context.getCurrent().welcome();
-    
-    while (context.isDone()==false) {
+    //initRegistry();
+    //context.getCurrent().displayRoom();
+
+            inventory.add("plastik");
+            inventory.add("metal");
+            inventory.add("giftigt affald");
+            inventory.add("plastik");
+            inventory.add("metal");
+            inventory.add("metal");
+
+    trash.sortTrash();
+
+   /* while (context.isDone()==false) {
       System.out.print("> ");
       String line = scanner.nextLine();
       registry.dispatch(line);
     }
     System.out.println("Game Over 😥");
+    */
   }
 }
