b7863df7fc9ead4a384fd57f7957c6b271b52935
Lalor19 <lauralorenzen4@gmail.com>
2023-11-14 17:05:46 +0100

    Tasks

--- Tasks.java
+++ Tasks.java
@@ -5,44 +5,32 @@ public class Tasks {
     String description;
     String rewardMsg;
     boolean isComplete = false;
-    private int timers;
-    private int trashCount;
+    private int timers = 30;
     private int taskCount;
+    static boolean timeUp = false;
 
 
     public Tasks (String description, String rewardMsg) {
         // initialize the taskCount, trashCount and timer as needed.
         this.description = description;
         this.rewardMsg = rewardMsg;
-        taskCount = 0 ;
-        trashCount = 0;
-        timers = 30;
+        if (getName().equals("oilRig")) {
+            startTimer();
+        }
     }
 
-    public String getTaskMessage() {
-        if (isComplete) return "\nOpgave: " + rewardMsg;
-        else return "\nOpgave: " + description;
-    }
 
     public String getDescription() {
         return description;
     }
-    public void taskComplete () {
-        // Kaldes når opgaven er udført
-        if (trashCount == 10 ) {
+
+
+    public void taskComplete () { // Kaldes når opgaven er udført
             // taskCount stiger når en task er complete
             taskCount++;
-            // trashCount resets when task is complete
-            trashCount = 0;
-
-        }
-        isComplete = true;
+            System.out.println(rewardMsg);
     }
 
-    public void trashCollected () {
-        // When player collects trash, call this method to increment trashCount
-        trashCount++;
-    }
 
     public void startTimer (){
         Timer timer = new Timer();
@@ -51,11 +39,14 @@ public class Tasks {
             @Override
             public void run() {
                 timers--;
+                if (timers != 0) {
+                    System.out.print("Du har " + timers + " sekunder tilbage");
+                }
                 if (timers == 0){
                     System.out.println("Time is up! :)");
                     taskComplete();
                     timer.cancel();
-
+                    timeUp = true;
                 }
             }
         };
